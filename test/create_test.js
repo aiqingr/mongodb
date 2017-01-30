084a135597c8e359c2a0c1815adf500736783127
Ni Tongyang <ntyaiqingr@gmail.com>
2017-01-30 17:27:37 -0500

    clean the database everytime bofore the testing & using done function to make sure to keep going after finishing one test

--- test/create_test.js
+++ test/create_test.js
@@ -2,9 +2,14 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Creating records', () => {
-  it('saves a user', () => {
+  it('saves a user', (done) => {
     const yoyo = new User({name: 'Yoyo'});
 
-    yoyo.save();
+    yoyo.save()
+      .then(() => {
+        //Has yoyo been saved successfully?
+        assert(!yoyo.isNew);
+        done();
+      });
   });
 });
